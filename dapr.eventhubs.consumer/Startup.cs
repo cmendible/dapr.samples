dc3a86bb9b2627259441cefbf8b287b61b53fa2b
cmendible <cmendible@gmail.com>
2020-02-24 00:16:02 +0100

    consumer

---
+++ dapr.eventhubs.consumer/Startup.cs
@@ -0,0 +1,76 @@
+using System;
+using System.Net.Http;
+using System.Text.Json;
+using System.Threading.Tasks;
+using Dapr;
+using Microsoft.AspNetCore.Builder;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.AspNetCore.Http;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Hosting;
+
+namespace DaprIntro
+{
+    public class Startup
+    {
+        /// <summary>
+        /// State store name.
+        /// </summary>
+        public const string StoreName = "statestore";
+
+        public Startup(IConfiguration configuration)
+        {
+            Configuration = configuration;
+        }
+
+        public IConfiguration Configuration { get; }
+
+        // This method gets called by the runtime. Use this method to add services to the container.
+        public void ConfigureServices(IServiceCollection services)
+        {
+            services.AddDaprClient();
+
+            services.AddHttpClient();
+
+            services.AddSingleton(new JsonSerializerOptions()
+            {
+                PropertyNamingPolicy = JsonNamingPolicy.CamelCase,
+                PropertyNameCaseInsensitive = true,
+            });
+        }
+
+        // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
+        public void Configure(IApplicationBuilder app, IWebHostEnvironment env, JsonSerializerOptions serializerOptions, IHttpClientFactory clientFactory)
+        {
+            if (env.IsDevelopment())
+            {
+                app.UseDeveloperExceptionPage();
+            }
+
+            app.UseRouting();
+
+            app.UseCloudEvents();
+
+            app.UseEndpoints(endpoints =>
+            {
+                endpoints.MapSubscribeHandler();
+
+                endpoints.MapPost("readings", Readings).WithTopic("readings");
+            });
+
+            async Task Readings(HttpContext context)
+            {
+                var message = await JsonSerializer.DeserializeAsync<dynamic>(context.Request.Body, serializerOptions);
+                if (string.IsNullOrEmpty(message))
+                {
+                    message = "nil";
+                }
+                Console.WriteLine(message.ToString());
+
+                context.Response.ContentType = "application/json";
+                await JsonSerializer.SerializeAsync(context.Response.Body, message.ToString(), serializerOptions);
+            }
+        }
+    }
+}
